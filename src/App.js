05801f3a459d36774c393b9f8954d470b0f250fb
kaancent <34494493+kaancent@users.noreply.github.com>
2024-01-22 04:20:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,15 +6,6 @@ import MovieCard from "./MovieCard";
 
 const API_URL = "http://www.omdbapi.com?apikey=2da7e8bc";
 
-const movie1 = {
-  Title: "Shrek",
-  Year: "2001",
-  imdbID: "tt0126029",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-};
-
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
